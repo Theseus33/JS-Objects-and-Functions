952ac7576293e853c9572ad2211c87b6fe863fb9
Rana Bhattacharyya <ranab33@gmail.com>
2019-05-24 18:22:42 -0400

    functions and objects

---
+++ script.js
@@ -0,0 +1,7 @@
+/**
+ * Object Oriented Programming
+ * 
+ * Objects interacting with one another through methods and properties
+ * 
+ * Used to store data, structure applications into modules and keeping the code clean.
+ */
