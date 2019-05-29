75e359092c1ea8f6abcd2915cdd6f8d65e51f7e4
Rana Bhattacharyya <ranab33@gmail.com>
2019-05-29 19:49:54 -0400

    closures

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/**
+`/**
  * Object Oriented Programming
  * 
  * Objects interacting with one another through methods and properties
@@ -147,8 +147,7 @@ We can store functions in a variable.
 We can pass a function as an argument to another function.
 We can return a function from a function
 -> first class functions
- */
-/*
+
 //Passing functions as Arguments
 
 var years = [1990, 1965, 1937, 2005, 1998];
@@ -207,7 +206,8 @@ function interviewQuestion(job) {
     };
   }
 }
-
+*/
+/*
 var teacherQuestion = interviewQuestion("teacher");
 var designerQuestion = interviewQuestion("designer");
 
@@ -225,25 +225,86 @@ function game() {
 }
 
 game();
-
+*/
 //done as an IIFE
 //in JS whats inside of parenthesis cannot be a statement
 //JS knows to treat this as an expression and not as a declaration
-
+/*
 (function() {
   var score = Math.random() * 10;
   console.log(score >= 5);
 })();
 
 console.log(score); //shows as undefined
-
+*/
 //we obtain data privacy and dont interfere with variables in the global scope
 //we can only call the IIFE once since its not tied to a variable
 //we arent using the IIFE to create a reusable function but
 //but create a new score hidden from the outside scope.
-
+/*
 (function(goodLuck) {
   var score = Math.random() * 10;
   console.log(score >= 5 - goodLuck);
 })(5);
+
 */
+// CLOSURES
+
+function retirement(retirementAge) {
+    var a = ' years left until retirement.';
+    return function(yearOfBirth) {
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
+}
+
+var retirementUS = retirement(66);
+retirementUS(1990);
+
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementGermany(1990);
+retirementUS(1990);
+retirementIceland(1990);
+
+
+//retirement(66)(1990);
+
+//Closure: An inner function always has access to the variables and parameters dof the outer function;
+
+//Rewriting the interview questions as closures;
+//original
+function interviewQuestion(job) {
+    if (job === "designer") {
+      return function(name) {
+        console.log(name + ", can you please explain what UX design is?");
+      };
+    } else if (job === "teacher") {
+      return function(name) {
+        console.log("What subject do you teach, " + name + "?");
+      };
+    } else {
+      return function(name) {
+        console.log("Hello " + name + ", what do you do?");
+      };
+    }
+  }
+  */
+
+  //with closures
+
+  function interviewQuestion(job) {
+      return function(name) {
+          if (job === 'designer') {
+            console.log(name + ", can you please explain what UX design is?");
+          }
+      } else if (job === 'teacher') {
+            console.log("What subject do you teach, " + name + "?");
+      } else {
+            console.log("Hello " + name + ", what do you do?");
+      }
+  }
+}
+
+interviewQuestion('teacher')('John');
