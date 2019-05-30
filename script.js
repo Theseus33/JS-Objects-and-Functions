78c43a50f5b5fd1022ebcd75753fc371ce74f1dc
Rana Bhattacharyya <ranab33@gmail.com>
2019-05-29 20:34:43 -0400

    quiz game setup

--- script.js
+++ script.js
@@ -380,3 +380,41 @@ function isFullAge(el) {
 var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
 console.log(fullJapan);
 console.log(ages);
+
+/////////////////////
+// CODING Challenge
+
+/*
+--- Lets build a fun quiz game in the console.! ---
+
+1. Build a function constructor called Question to describe a question. A question should include: 
+a) the question itself
+b) the answers from which the player can choose the correct one (chose an adequate data structure here, array object, etc.)
+c. correct answer (I would use a number for this)
+
+2. Create a couple of questions using the constuctor
+
+3. Store them all inside an array
+
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint:
+write a method for the Question objects with this task).
+
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the cirrect anseer such as you 
+displayed it in Task 4.
+
+6. Check if the answer is correct and print to the console whether the answer is correct or not (Hint: write another method for this)
+
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't
+interfere with the other programmers code. 
+
+--- Expert Level ---
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function forr this and call 
+it right after displaying the result)
+
+9. Be careful: after Task 8, the game literally never ends. SO include the option to quit the game if the user writes 'exit' instead of the 
+answer. In this case, DONT call the function from task 8.
+
+1-. Track the user's score to make the game more fun! So each time an answer is correct, add 1 poiint to the score. (Hint: I'm going to use
+the power of closure sfor this, but you dont have to ), just do this with the tools you feel most comforatble with.
+
+11. display the score in teh console. Use yet anotehr method for this.
